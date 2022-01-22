4f7474c5e006e0fcfc50babe6288b9b7d0434c4b
Danilo Kodavara <danilo.kodavara@hotmail.com>
2022-01-22 12:45:21 -0300

    Aprendendo a tratar erros em go.

--- hello.go
+++ hello.go
@@ -1,10 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	res, err := http.Get("http://google.com.br")
 	if err != nil {
-		return
+		log.Println(err.Error())
+	} else {
+		fmt.Println(res.Header)
 	}
+
+	resSoma, errSoma := soma(10, 10)
+	if errSoma != nil {
+		log.Fatal(errSoma.Error())
+	}
+
+	fmt.Println(resSoma)
+
+}
+
+func soma(x int, y int) (int, error) {
+	res := x + y
+	if res > 10 {
+		return 0, errors.New("Total maior que 10")
+	}
+
+	return res, nil
 }
